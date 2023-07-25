b107b3492c3f31b8eb53beb31a8ee3489636e3ab
MatheusGorga <matheusgobar@gmail.com>
2023-07-25 14:57:51 -0300

    up

---
+++ src/pages/Cardapio/index.tsx
@@ -0,0 +1,13 @@
+import styles from "./Cardapio.module.scss";
+
+function Cardapio() {
+    return ( 
+        <main>
+        <nav className={styles.menu}>
+        <img src="../../assets/logo.svg" alt="logo do aluroni" />
+        </nav>
+      </main>
+     );
+}
+
+export default Cardapio;
