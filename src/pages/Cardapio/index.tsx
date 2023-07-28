66a35816bced768f55cb58bcdf0e4d55cc3ec053
MatheusGorga <matheusgobar@gmail.com>
2023-07-28 11:57:58 -0300

    up

--- src/pages/Cardapio/index.tsx
+++ src/pages/Cardapio/index.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import Buscador from "./Buscador";
 import styles from "./Cardapio.module.scss";
 import {ReactComponent as Logo} from 'assets/logo.svg'
+import Filtros from "./Filtros";
 
 function Cardapio() {
 
@@ -25,6 +26,9 @@ function Cardapio() {
 
           <Buscador busca={busca} setBusca={setBusca}/>
 
+          <div className={styles.cardapio__filtros}>
+            <Filtros/>
+          </div>
         </section>
       </main>
      );
