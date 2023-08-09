5f3b2dab23cbbdeb0a8343e661c7d0a593b61963
MatheusGorga <matheusgobar@gmail.com>
2023-08-09 19:57:26 -0300

    up

---
+++ src/components/Menu/index.tsx
@@ -0,0 +1,33 @@
+import { Link } from 'react-router-dom';
+import styles from './Menu.module.scss';
+import { ReactComponent as Logo } from 'assets/logo.svg';
+
+function AppBar() {
+  const rotas = [{
+    label: 'Inicio',
+    to: '/'
+  },{
+    label: 'Cardapio',
+    to: '/cardapio'
+  },{
+    label: 'Sobre',
+    to: '/sobre'
+  }];
+  return (
+    <nav className={styles.menu}>
+      <Logo />
+
+      <ul className={styles.menu__list}>
+        {rotas.map((rota, index) => (
+          <li key={index}  className={styles.menu__link}>
+            <Link to={rota.to}>
+              {rota.label}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+}
+
+export default AppBar;
--- src/routes.js
+++ src/routes.js
@@ -1,15 +1,24 @@
+import AppBar from 'components/Menu';
 import Cardapio from 'pages/Cardapio';
 import Inicio from 'pages/Inicio';
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import styles from './Routes.module.scss';
 
 function AppRouter() {
-  return ( 
-    <Router>
-      <Routes>
-        <Route path='/'  element={<Inicio/>}/>
-        <Route path='/cardapio'  element={<Cardapio/>}/>
-      </Routes>
-    </Router>
+  return (
+    <main>
+      <Router>
+        <AppBar />
+        <header className={styles.header}>
+          <div className={styles.header__text}>A casa da massa</div>
+        </header>
+
+        <Routes>
+          <Route path="/" element={<Inicio />} />
+          <Route path="/cardapio" element={<Cardapio />} />
+        </Routes>
+      </Router>
+    </main>
   );
 }
 
