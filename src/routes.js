c42a852f041e8a85899d804dc70fe35d464f9eb7
MatheusGorga <matheusgobar@gmail.com>
2023-08-12 11:38:53 -0300

    up

--- src/routes.js
+++ src/routes.js
@@ -4,10 +4,13 @@ import Inicio from 'pages/Inicio';
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import PaginaPadrao from 'components/PaginaPadrao';
 import Sobre from 'pages/Sobre';
+import Footer from 'components/Footer';
+import NotFound from 'pages/NotFound';
+import './index.css';
 
 function AppRouter() {
   return (
-    <main>
+    <main className='container'>
       <Router>
         <AppBar />
         <Routes>
@@ -15,9 +18,10 @@ function AppRouter() {
             <Route index element={<Inicio/>}/>
             <Route path="cardapio" element={<Cardapio />} />
             <Route path='sobre' element={<Sobre />} /> 
-          </Route>
-          
+          </Route>   
+          <Route path='*' element={<NotFound/>}/>
         </Routes>
+        <Footer/>
       </Router>
     </main>
   );
